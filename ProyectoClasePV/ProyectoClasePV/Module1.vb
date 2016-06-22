5a003ac76dfb0b2a9ad40cad6064051d717b4df0
loreliji <loreliji@gmail.com>
2016-06-22 12:53:35 -0500

    Cambios de Loreliji

--- ProyectoClasePV/ProyectoClasePV/Module1.vb
+++ ProyectoClasePV/ProyectoClasePV/Module1.vb
@@ -3,6 +3,7 @@
     Sub Main()
         Console.WriteLine("hola mundo")
         Console.WriteLine("Cambios desde VS2013")
+        Console.WriteLine("Prueba de edición - Lú")
 
         Console.ReadLine()
     End Sub
