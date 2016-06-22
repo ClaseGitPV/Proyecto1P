5402084ef9b0ee5711b8a3da7efe5ed42321d1a5
Khehylaicaza <Mikeila7>
2016-06-22 12:55:06 -0500

    Nuevo Mensaje

--- ProyectoClasePV/ProyectoClasePV/Module1.vb
+++ ProyectoClasePV/ProyectoClasePV/Module1.vb
@@ -3,6 +3,17 @@
     Sub Main()
         Console.WriteLine("hola mundo")
         Console.WriteLine("Cambios desde VS2013")
+
+
+
+
+
+
+
+
+
+
+
         Console.WriteLine("Lalalalalalala----k")
 
         Console.ReadLine()
