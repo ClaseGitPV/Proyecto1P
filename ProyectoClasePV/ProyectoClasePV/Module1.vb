8ac184f08e99167b8308a61a5f07e45b9440b950
yandersantana <ysantanatavares.1996@hotmail.com>
2016-06-22 12:56:06 -0500

    modificado por : yander santana

--- ProyectoClasePV/ProyectoClasePV/Module1.vb
+++ ProyectoClasePV/ProyectoClasePV/Module1.vb
@@ -3,6 +3,13 @@
     Sub Main()
         Console.WriteLine("hola mundo")
         Console.WriteLine("Cambios desde VS2013")
+
+
+
+
+
+
+
         Console.WriteLine("Comprobando cambios .....Yander Santana")
         Console.ReadLine()
     End Sub
